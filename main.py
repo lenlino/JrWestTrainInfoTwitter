d0cfedd9f8528b5cf885517982bced7ccfd5dcee
lenlino <lenlino@hotmail.com>
2024-06-17 14:51:20 +0900

    新規にWebスクレイピングのスクリプトを作成
    main.pyにて、"https://trafficinfo.westjr.co.jp/kinki.html"のHTMLを取得し解析する新規スクリプトを追加しました。
    該当ページ内の指定されたセクションから情報を取り出し、出力する処理を実装しています。

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+# HTMLの取得(GET)
+req = requests.get("https://trafficinfo.westjr.co.jp/kinki.html")
+req.encoding = req.apparent_encoding # 日本語の文字化け防止
+
+# HTMLの解析
+bsObj = BeautifulSoup(req.text,"html.parser")
+
+items = bsObj.find(id="syosai_4").find_all(class_="jisyo_contents")
+
+for item in items:
+    contents = item.find_all("p")
+    for content in contents:
+        print(content.text)
+
+
